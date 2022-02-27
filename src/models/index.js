ed4e910dcfd272220c5ebacacda4f98003e0d85d
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-02-27 22:20:47 +0300

    clas03

--- src/models/index.js
+++ src/models/index.js
@@ -6,9 +6,16 @@ require('dotenv').config();
 
 
 const myPOSTGRES_URL = process.env.DATABASE_URL;
-// let sequelizeOptions =  {}
+let sequelizeOptions =  {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+  }
 
-  let sequelize = new Sequelize(myPOSTGRES_URL,{});
+  let sequelize = new Sequelize(myPOSTGRES_URL,sequelizeOptions);
 
 
   module.exports = {
